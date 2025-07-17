b2f86e3aca314e0c00fc70df9e7e464d2a81ba39
jabir <jabir.mdn@gmail.com>
2025-07-17 10:57:36 +0530

    Finished working on the palindrome method

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,4 +3,8 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "jabirmdn_palindrome"
 
+require "minitest/spec"
 require "minitest/autorun"
+require "minitest/reporters"
+
+Minitest::Reporters.use!
